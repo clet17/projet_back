9c3f10ff48c15bf47163e4f5fa756a0f64023eaa
Toni <fazendeirotoni@gmail.com>
2025-07-07 21:17:58 +0200

    test2

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,6 +1,13 @@
-import Stripe from 'stripe'
+import StripePkg from 'stripe'
+const Stripe = StripePkg.default || StripePkg
 
-console.log('clé Stripe:', process.env.STRIPE_SECRET_KEY)
+if (!process.env.STRIPE_SECRET_KEY) {
+  console.log('⚠️ STRIPE_SECRET_KEY manquante dans process.env')
+  throw new Error('Clé Stripe non définie dans les variables d’environnement')
+} else {
+  console.log('✅ Clé Stripe chargée avec succès')
+}
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export const createPaymentIntent = async (req, res) => {
