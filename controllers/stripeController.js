dc78f237a432ce6004a029ccc30706de26ad27a9
Toni <fazendeirotoni@gmail.com>
2025-07-07 21:02:38 +0200

    test

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,5 +1,6 @@
 import Stripe from 'stripe'
 
+console.log('clé Stripe:', process.env.STRIPE_SECRET_KEY)
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export const createPaymentIntent = async (req, res) => {
